35bd082d0ee8d1cf935a6e75a77e6a3edda97f24
Darlene Zouras <dzouras@gmail.com>
2017-04-13 21:47:05 -0500

    ready to test adventurer

--- projects/zourasd/dominion/dominion.c
+++ projects/zourasd/dominion/dominion.c
@@ -668,27 +668,9 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
     case smithy:
         playSmithy(state, handPos, currentPlayer);
 
-        
+
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+        playAdventurer(state, currentPlayer, drawntreasure, temphand)
 
     case council_room:
       //+4 Cards
@@ -1318,6 +1300,32 @@ int updateCoins(int player, struct gameState *state, int bonus)
   state->coins += bonus;
 
   return 0;
+}
+
+playAdventurer(struct gameState *state, int currentPlayer, int drawntreasure, int* temphand) {
+    while(drawntreasure<2){
+        if (state->deckCount[currentPlayer] <1){
+            //if the deck is empty we need to shuffle discard and add to deck
+            shuffle(currentPlayer, state);
+        }
+        drawCard(currentPlayer, state);
+        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];
+        //top card of hand is most recently drawn card.
+
+        if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+            drawntreasure++;
+        else{
+            temphand[z]=cardDrawn;
+            state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+            z++;
+        }
+    }
+    while(z-1>=0){
+        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1];
+        // discard all cards in play that have been drawn
+        z=z-1;
+    }
+    return 0;
 }
 
 playSmithy(struct gameState *state, int handPos, int currentPlayer) {

a1179a1a3f2851b489c21552b4331dde077dac11
Darlene Zouras <dzouras@gmail.com>
2017-05-11 08:36:57 -0500

    working on assignment 4

---
+++ projects/zourasd/dominion/randomtestadventurer.c
@@ -0,0 +1,17 @@
+// random test on playAdventurer function (line 1268)
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+// int playAdventurer (struct gameState *state, int currentPlayer, int drawntreasure, int* temphand, int z, int cardDrawn)
+// returns 0 for a successful return
+int main() {
+    printf("\n###################\n");
+    printf("Card test (random test) - Adventurer\n");
+
+    // set function parameters
+    
+}
---
+++ projects/zourasd/dominion/randomtestcard1.c
@@ -0,0 +1,15 @@
+// random test on playSmithy funciton (line 1294)
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+// int playSmithy(struct gameState *state, int handPos, int currentPlayer)
+// returns 0 on normal exit
+// should add three cards to hand
+int main() {
+    printf("\n####################\n");
+    printf("Card test (random test) - Smithy\n");
+}

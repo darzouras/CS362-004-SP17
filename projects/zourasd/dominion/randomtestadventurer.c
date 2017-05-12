d23e0ef5dc700742794ee8cef85419e86f2d2a0b
Darlene Zouras <dzouras@gmail.com>
2017-05-12 08:33:28 -0500

    still working

--- projects/zourasd/dominion/randomtestadventurer.c
+++ projects/zourasd/dominion/randomtestadventurer.c
@@ -13,5 +13,41 @@ int main() {
     printf("Card test (random test) - Adventurer\n");
 
     // set function parameters
-    
+    int i, n, p, z, cardDrawn, numPlayer, drawntreasure, count, badcount, seed;
+    struct gameState G;
+    int tempHand[MAX_HAND];
+    int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
+    int handCount, deckCount;
+    int silvers[MAX_HAND];
+    int estates[MAX_HAND];
+    int adventurers[MAX_HAND];
+    for (i = 0; i < MAX_HAND; i++) {
+        silvers[i] = silver;
+        estates[i] = estate;
+        adventurers[i] = adventurer;
+    }
+
+    // test multiple players with multiple seeds
+    for (n = 0; n < 10; n++) {
+        // testing for multiple hand counts
+        for (handCount = 1; handCount < MAX_HAND; handCount++) {
+            // also for multiple deck counts
+            for (deckCount = 1; deckCount < MAX_DECK; deckCount++) {
+                // and multiple players 
+                for (p = 0; p < MAX_PLAYERS; p++) {
+                    printf("\n--- Testing player %d\n", p);
+
+                    count = 0;
+                    badcount = 0;
+                    for (i = 0; i = )
+                    // clear gameState, set the hand
+                    memset(&G, 23, sizeof(struct gameState));
+                    intiializeGame(numPlayer, k, n, &G);
+
+
+                }
+            }
+        }
+    }
+
 }
